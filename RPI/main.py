7f10940c8380dc50d15364ef4d0208041831e0f9
Jan Podávka <72079041+JanPodavka@users.noreply.github.com>
2024-03-02 01:21:44 +0100

    api setup

--- RPI/main.py
+++ RPI/main.py
@@ -1,2 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+# Define a route to receive control commands
+@app.route('/control', methods=['POST'])
+def control_car():
+    command = request.form.get('command')
+    print("Received command:", command)
+
+    return 'OK', 200
+
+
 if __name__ == '__main__':
-    print("hello world !")
+    app.run(host='0.0.0.0', port=5000)
